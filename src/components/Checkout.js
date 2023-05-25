a7eee477399411660c04b8c32302aa2bff557f4c
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-05-26 02:42:54 +0530

    Added react CSSTransition+TransitionGroup and some custom Transition CSS

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,8 +6,11 @@ import Subtotal from './Subtotal'
 import { StateContext } from '../state/StateProvider'
 import Product from './Product'
 import CheckoutProduct from './CheckoutProduct'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+
 const Checkout = () => {
-  const {basket} = useContext(StateContext)
+  const {basket, user} = useContext(StateContext)
   return (
     <>
      <Header/>
@@ -16,20 +19,25 @@ const Checkout = () => {
       <div className="checkout_left">
         
         <div>
+        <h3>Hello, {user?.email}</h3>
           <h2 className="checkout_title">
             Your Shopping Basket
-           
+            <TransitionGroup className="checkout_items">
              { basket.map(product=>{
-               return(<CheckoutProduct  id={product.id}
+               return(
+                <CSSTransition key={Math.random()*10+100} classNames="checkout_item" timeout={300}>
+                    <CheckoutProduct key={product.id} id={product.id}
                             title={product.title}
                             image={product.image}
                              price={product.price}
                             rating={product.rating}
                             isCheckout={true}
-                           />)
+                           />
+                </CSSTransition>)
             })
        
             }
+            </TransitionGroup>
           </h2>
         </div>
     </div>
